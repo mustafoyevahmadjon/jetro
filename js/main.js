08396d71667f5d1572a4dcddaed6ad6dea4506b5
ahmadjon-coder-creator <mustafayevahmadjon@gmail.com>
2021-02-24 18:10:28 +0500

    add

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function(){
+    $('.slider').slick({
+        arrows: true,
+        asNavFor: '.thumbs',
+        // autoplay: true,
+        //  autoplaySpeed: 1000,
+         prevArrow:  '<button class="slick-arrow slick-prev"><img src="images/slider/arrow_left.svg" alt=""></button>',
+         nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/arrow_right.svg" alt=""></button>',
+    });
+
+    $('.thumbs').slick({
+        arrows:false,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        asNavFor: '.slider',
+        focusOnSelect: true
+    });
+});
